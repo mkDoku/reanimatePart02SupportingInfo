7fe33950849aef37b94259a20c0acb0adc884a71
mkDoku <bubecksascha@t-online.de>
2020-12-19 10:18:08 +0100

    A static star animation
    	* added animation for a static star using mkLinePathClosed

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,37 @@
-module Main where
+#!/usr/bin/env stack
+-- stack runghc --package reanimate
+
+module Main ( main ) where
+
+import           Reanimate
+import           Reanimate.Builtin.Documentation
+
+customDuration = 1
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = reanimate
+  $ docEnv
+  $ staticFrame customDuration
+  $ staticStar
+
+-- A static 'SVG' by using 'mkLinePathClosed'
+--
+-- with 'mkLinePathClosed' the last point will
+-- be connected to the first point
+staticStar :: SVG
+staticStar =
+  mkLinePathClosed
+    [ leftBottom
+      , middleTop
+      , rightBottom
+      , leftCustom
+      , rightCustom
+    ]
+
+-- Some coordinates
+leftBottom   = (screenLeft, screenBottom)
+middleTop    = (0, screenTop)
+rightBottom  = (screenRight, screenBottom)
+customHeigth = 1
+leftCustom   = (screenLeft, customHeigth)
+rightCustom  = (screenRight, customHeigth)
